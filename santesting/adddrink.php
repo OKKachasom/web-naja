26db96403ac8b708c1085991ff21e79d146fa149
kornninikkon <59070018@it.kmitl.ac.th>
2018-12-15 22:55:33 +0700

    finish drink

--- santesting/adddrink.php
+++ santesting/adddrink.php
@@ -1,9 +1,10 @@
 <?php
-
+header('Content-Type: text/html; charset=utf-8');
 
 $inputName = filter_input(INPUT_POST, 'inputName');
 $inputEmail = filter_input(INPUT_POST, 'inputEmail');
 $inputTel = filter_input(INPUT_POST, 'inputTel');
+//$inputOrder = filter_var(FILTER_SANITIZE_STRING, 'order')
 
 
 
@@ -15,11 +16,14 @@ $inputTel = filter_input(INPUT_POST, 'inputTel');
 		$dbname = "okkachasom_san";
 		$conn = new mysqli($servername, $username, $password, $dbname);
 
+		mysqli_set_charset($conn, "utf8");
+
 		if (mysqli_connect_error()){
   			die('Connect Error ('. mysqli_connect_errno() .') ' . mysqli_connect_error());
 }
 		else{
 			$sql = "INSERT INTO drink_data (inputName,inputEmail,inputTel) values('$inputName','$inputEmail','$inputTel')";
+			//$sql = "INSERT INTO drink_data (inputName,inputEmail,inputTel,inputOrder) values('$inputName','$inputEmail','$inputTel','&inputOrder')";
 			if ($conn->query($sql)){
 				
     echo "Your order have been sent! Thank you!";
@@ -30,6 +34,7 @@ $inputTel = filter_input(INPUT_POST, 'inputTel');
     echo "<br>";
     echo "\nYour Tel.number: " . $inputTel;
     echo "<br>";
+    //echo "\nYour Order: " . $inputOrder;
   }
   else{
     echo "Error: ". $sql ." ". $conn->error;
