d1ceb1b8ed2ba5172088f32fa57d3a7783edcd5b
OKKachasom <59070001@kmitl.ac.th>
2018-12-15 15:19:30 +0700

    databaseeeee

--- santesting/adddrink.php
+++ santesting/adddrink.php
@@ -1,25 +1,37 @@
 <?php
 
+
+$inputName = filter_input(INPUT_POST, 'inputName');
+$inputEmail = filter_input(INPUT_POST, 'inputEmail');
+$inputTel = filter_input(INPUT_POST, 'inputTel');
+
+
+
+
+
 		$servername = "localhost";
 		$username = "okkachasom_san";
 		$password = "8Oit27SSQm";
 		$dbname = "okkachasom_san";
 		$conn = new mysqli($servername, $username, $password, $dbname);
 
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		} 
+		if (mysqli_connect_error()){
+  			die('Connect Error ('. mysqli_connect_errno() .') ' . mysqli_connect_error());
+}
 		else{
-			return $conn;
-		}
+			$sql = "INSERT INTO drink_data (inputName,inputEmail,inputTel) values('$inputName','$inputEmail','$inputTel')";
+			if ($conn->query($sql)){
+    echo "Your order have been sent! Thank you!";
+  }
+  else{
+    echo "Error: ". $sql ."
+". $conn->error;
+  }
+  $conn->close();
+}
 
 
 
-
-//Block 6
-echo 'You have been added.';
-
-//Block 7
-mysql_close($connection);
+// mysql_close($connection);
 
 ?>
